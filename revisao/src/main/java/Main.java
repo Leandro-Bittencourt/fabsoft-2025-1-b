87f7f34f28faaa388bbfda88ac85d52f7e9f4e3f
Leandro-Bittencourt <leandro.balansin@univille.br>
2025-03-18 20:30:59 -0300

    revisao

--- revisao/src/main/java/Main.java
+++ revisao/src/main/java/Main.java
@@ -1,14 +1,28 @@
+import br.univille.entity.Cliente;
+import br.univille.entity.Pokemon;
+import br.univille.entity.Cidade;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
-        Cliente zezinho = new Cliente("Zezinho");
-        zezinho.setNome("Zezinho da silva");
+        Cidade joinville = new Cidade();
+        joinville.setNome("Joinville");
+        joinville.setEstado("Santa Catarina");
+
+        Pokemon richard = new Pokemon("Richard");
+
+        Cliente leandro = new Cliente("Leandro");
+        leandro.setNome("Leandro Bittencourt");
+        leandro.setCidade(joinville);
+
+        leandro.getListaPokemon().add(richard);
+
         Cliente mariazinha = new Cliente("Mariazinha");
         mariazinha.setNome("Mariaznha");
 
 
-        System.out.println(zezinho);
+        System.out.println(leandro);
         System.out.println(mariazinha);
     }
 }
---
+++ revisao/src/main/java/br/univille/entity/Cliente.java
@@ -0,0 +1,68 @@
+package br.univille.entity;
+
+import java.util.ArrayList;
+
+public class Cliente {
+    //variável - atributi
+    private long id;
+    private String nome;
+    private String endereço;
+
+    private ArrayList listaPokemon = new ArrayList();
+
+    public ArrayList getListaPokemon() {
+        return listaPokemon;
+    }
+    public void serListaPokemon(ArrayList listaPokemon){
+        this.listaPokemon = listaPokemon;
+    }
+
+    private Cidade cidade;
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEndereço() {
+        return endereço;
+    }
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    //construtor - mesmo nome da classe e não tem retorno
+    //2 funções - inicializar atributos
+    //obrigar a passagem de valores
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    public Cliente() {
+        //polimorfismo - várias formas de existir a mesma coisa
+    }
+
+    //método
+    //sobreescrita de método(meu pai me deu pronto e eu zuera reescrevi) 
+    @Override
+    public String toString(){
+        return getNome();
+        //quando da F5 os nomes aparecem, ao invés de um código
+    }
+
+    //método
+    public String getNome() {return nome;
+    
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+ 
+ }
