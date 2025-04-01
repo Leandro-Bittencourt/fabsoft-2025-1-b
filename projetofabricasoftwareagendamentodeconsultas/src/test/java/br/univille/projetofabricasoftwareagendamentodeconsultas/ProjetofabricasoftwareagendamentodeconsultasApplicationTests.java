99fa5c8f32fd38363b6acc359a09a71322bba6b4
Leandro-Bittencourt <leandro.balansin@univille.br>
2025-04-01 20:26:21 -0300

    inicio projeto

---
+++ projetofabricasoftwareagendamentodeconsultas/src/test/java/br/univille/projetofabricasoftwareagendamentodeconsultas/ProjetofabricasoftwareagendamentodeconsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabricasoftwareagendamentodeconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofabricasoftwareagendamentodeconsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
