3247b791f477ecff82f307383774f596b02edb4a
Leandro-Bittencourt <leandro.balansin@univille.br>
2025-05-06 20:15:06 -0300

    dockerfile

--- projetofabricasoftwareagendamentodeconsultas/src/main/java/br/univille/projetofabricasoftwareagendamentodeconsultas/entity/Consulta.java
+++ projetofabricasoftwareagendamentodeconsultas/src/main/java/br/univille/projetofabricasoftwareagendamentodeconsultas/entity/Consulta.java
@@ -2,52 +2,71 @@ package br.univille.projetofabricasoftwareagendamentodeconsultas.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Consulta {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Future(message = "A data da consulta deve ser no futuro")
+    @Column(nullable = false)
     private LocalDateTime dataHora;
+
     @ManyToOne
+    @NotNull
     private Paciente paciente;
+
     @ManyToOne
+    @NotNull
     private Medico medico;
+
+    @NotNull
+    @Column(nullable = false)
     private String status;
 
     // Getters e Setters
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public LocalDateTime getDataHora() {
         return dataHora;
     }
+
     public void setDataHora(LocalDateTime dataHora) {
         this.dataHora = dataHora;
     }
+
     public Paciente getPaciente() {
         return paciente;
     }
+
     public void setPaciente(Paciente paciente) {
         this.paciente = paciente;
     }
+
     public Medico getMedico() {
         return medico;
     }
+
     public void setMedico(Medico medico) {
         this.medico = medico;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
