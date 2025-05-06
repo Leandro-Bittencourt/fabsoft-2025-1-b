b68b1e5c940f9cb6f5e5f1a60f5805707797e07d
Leandro-Bittencourt <leandro.balansin@univille.br>
2025-05-06 19:10:09 -0300

    api

--- projetofabricasoftwareagendamentodeconsultas/src/main/java/br/univille/projetofabricasoftwareagendamentodeconsultas/controller/MedicoController.java
+++ projetofabricasoftwareagendamentodeconsultas/src/main/java/br/univille/projetofabricasoftwareagendamentodeconsultas/controller/MedicoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/medicos")
+@RequestMapping("/api/medico")
 public class MedicoController {
 
     @Autowired
