9848f3c22340b315269438ba1c6fecc99315a7ef
Leandro-Bittencourt <147086907+Leandro-Bittencourt@users.noreply.github.com>
2025-05-27 23:38:46 +0000

    frontend

--- projetofabsoft-frontend/src/app/app.routes.ts
+++ projetofabsoft-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PacienteComponent }   from './paciente/paciente.component';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [];
+{ path: 'pacientes', Component: PacienteComponent}
+];
--- projetofabsoft-frontend/src/app/paciente/paciente.component.ts
+++ projetofabsoft-frontend/src/app/paciente/paciente.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Paciente } from '../model/paciente';
+import { PacienteService } from '../service/paciente.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-paciente',
-  imports: [],
+  imports: [ HttpClientModule, CommonModule ],
   templateUrl: './paciente.component.html',
-  styleUrl: './paciente.component.css'
+  styleUrl: './paciente.component.css',
+  providers: [PacienteService]
 })
 export class PacienteComponent {
+  listaPacientes: Paciente[] = [];
 
+  constructor(private pacienteService: PacienteService) {}
+
+  ngOnInit(){
+    console.log("Carregando pacientes...");
+    this.pacienteService.getPacientes().subscribe(
+      pacientes => {
+        this.listaPacientes = pacientes;
+      }
+    )
+  }
 }
